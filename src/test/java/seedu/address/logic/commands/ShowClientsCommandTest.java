2913953e7bedba1ed1aa5815ce7315ffedc81c75
FlyingMun <wengmunchia@gmail.com>
2025-10-16 00:55:14 +0800

    Add getPropertyById method to ModelStub after upstream merge

--- src/test/java/seedu/address/logic/commands/ShowClientsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ShowClientsCommandTest.java
@@ -252,6 +252,11 @@ public class ShowClientsCommandTest {
         }
 
         @Override
+        public Property getPropertyById(String id) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public void updateFilteredPersonList(Predicate<Person> predicate) {
             throw new AssertionError("This method should not be called.");
         }
