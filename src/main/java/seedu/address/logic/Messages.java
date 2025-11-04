1024d0d738c2790a5377afedb17c76e632285585
joojaja <194677730+joojaja@users.noreply.github.com>
2025-11-04 08:01:48 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -22,7 +22,8 @@ public class Messages {
             "Multiple values specified for the following single-valued field(s): ";
     public static final String MESSAGE_INVALID_PROPERTY_DISPLAYED_INDEX = "A property index provided is invalid";
     public static final String MESSAGE_INVALID_RELATIONSHIP = "The relationship provided is invalid";
-    public static final String MESSAGE_INVALID_PROPERTY_DISPLAYED_ID = "A property ID provided is either invalid or is not shown on the UI";
+    public static final String MESSAGE_INVALID_PROPERTY_DISPLAYED_ID = "A property ID provided is either invalid"
+                                                                        + "or is not shown on the UI";
     public static final String MESSAGE_LINKING_ALREADY_LINKED_BUYER =
             "A contact is already linked to one of the properties as buyer";
     public static final String MESSAGE_LINKING_ALREADY_LINKED_SELLER =
